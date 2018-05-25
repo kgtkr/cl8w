3bd474582f8c95475d43cadd8b217dc7d4068665
tkr <kgtkr.jp@gmail.com>
2018-05-25 21:02:57 +0900

    defとか

--- app/Parsers.hs
+++ app/Parsers.hs
@@ -1,6 +1,61 @@
 module Parsers where
 
+import           Text.ParserCombinators.Parsec
+import           Text.ParserCombinators.Parsec.Language
+
 type Ident=String
+
+keywords =
+  [ "i32"
+  , "i64"
+  , "f32"
+  , "f64"
+  , "string"
+  , "bool"
+  , "char"
+  , "true"
+  , "false"
+  , "null"
+  , "let"
+  , "if"
+  , "while"
+  , "return"
+  , "struct"
+  , "fun"
+  , "extern"
+  ]
+
+def :: LanguageDef st
+def = LanguageDef
+  { commentStart    = "/*"
+  , commentEnd      = "*/"
+  , commentLine     = "//"
+  , nestedComments  = True
+  , identStart      = letter
+  , identLetter     = alphaNum <|> oneOf "_"
+  , opStart         = oneOf "+-*/%&|^=<>!"
+  , opLetter        = oneOf "+-*/%&|^=<>!"
+  , reservedOpNames = [ "+"
+                      , "-"
+                      , "*"
+                      , "/"
+                      , "%"
+                      , "&&"
+                      , "||"
+                      , "&"
+                      , "|"
+                      , "^"
+                      , "**"
+                      , "=="
+                      , "<"
+                      , "<="
+                      , ">"
+                      , ">="
+                      , "!"
+                      ]
+  , reservedNames   = keywords
+  , caseSensitive   = True
+  }
 
 data Type = TI32
           | TI64
@@ -66,4 +121,3 @@ data FuncDef=FuncDef{
 data Member=MStruct Ident [(Ident,Type)]
             |MFun FuncDef Stat
             |MExternFun FuncDef String
-
