f057ccb2f11361518d6ac2d1445d6de6e3fc09ac
tkr <kgtkr.jp@gmail.com>
2018-05-26 10:15:12 +0900

    langにラッパー

--- src/Parsers/Lang.hs
+++ src/Parsers/Lang.hs
@@ -43,6 +43,64 @@ def = LanguageDef
 
 tokenParser = P.makeTokenParser def
 
+identifier :: Parser String
+identifier = P.identifier tokenParser
+
+operator :: Parser String
+operator = P.operator tokenParser
+
+
+reserved :: String -> Parser ()
+reserved = P.reserved tokenParser
+
+charLiteral :: Parser Char
+charLiteral = P.charLiteral tokenParser
+
+stringLiteral :: Parser String
+stringLiteral = P.stringLiteral tokenParser
+
+integer :: Parser Integer
+integer = P.integer tokenParser
+
+float :: Parser Double
+float = P.float tokenParser
+
+whiteSpace :: Parser ()
+whiteSpace = P.whiteSpace tokenParser
+
+parens :: Parser a -> Parser a
+parens = P.parens tokenParser
+
+braces :: Parser a -> Parser a
+braces = P.braces tokenParser
+
+brackets :: Parser a -> Parser a
+brackets = P.brackets tokenParser
+
+semi :: Parser String
+semi = P.semi tokenParser
+
+comma :: Parser String
+comma = P.comma tokenParser
+
+colon :: Parser String
+colon = P.colon tokenParser
+
+dot :: Parser String
+dot = P.dot tokenParser
+
+semiSep :: Parser a -> Parser [a]
+semiSep = P.semiSep tokenParser
+
+semiSep1 :: Parser a -> Parser [a]
+semiSep1 = P.semiSep1 tokenParser
+
+commaSep :: Parser a -> Parser [a]
+commaSep = P.commaSep tokenParser
+
+commaSep1 :: Parser a -> Parser [a]
+commaSep1 = P.commaSep1 tokenParser
+
 type Ident=String
 
 data Type = TI32
