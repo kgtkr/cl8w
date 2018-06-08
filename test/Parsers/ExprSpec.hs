08f6a61ab47859e09080d67da53b6e8ce10f8db3
tkr <kgtkr.jp@gmail.com>
2018-06-08 17:01:38 +0900

    test

--- test/Parsers/ExprSpec.hs
+++ test/Parsers/ExprSpec.hs
@@ -8,6 +8,8 @@ spec :: Spec
 spec = do
   describe "exprP" $ do
     it "test" $ do
-      (parse exprP "test" "Struct {x:Foo{}}")
-        `shouldBe` Right (EStructL "Struct" [("x", EStructL "Foo" [])])
+      (parse exprP "test" "1") `shouldBe` Right (EI32L 1)
+      (parse exprP "test" "1i32") `shouldBe` Right (EI32L 1)
+      (parse exprP "test" "1i64") `shouldBe` Right (EI64L 1)
+      (parse exprP "test" "\"aiueo\\n\"") `shouldBe` Right (EStringL "aiueo\n")
 
