d47d7ff48619780a54507443bedc8da7c485d20f
tkr <kgtkr.jp@gmail.com>
2018-06-08 19:28:55 +0900

    配列テスト

--- test/Parsers/ExprSpec.hs
+++ test/Parsers/ExprSpec.hs
@@ -3,6 +3,7 @@ module Parsers.ExprSpec where
 import           Text.ParserCombinators.Parsec
 import           Test.Hspec
 import           Parsers.Expr
+import           Parsers.Lang
 
 spec :: Spec
 spec = do
@@ -12,4 +13,5 @@ spec = do
       (parse exprP "test" "1i32") `shouldBe` Right (EI32L 1)
       (parse exprP "test" "1i64") `shouldBe` Right (EI64L 1)
       (parse exprP "test" "\"aiueo\\n\"") `shouldBe` Right (EStringL "aiueo\n")
+      (parse exprP "test" "[i32;1]") `shouldBe` Right (EArrayL TI32 (EI32L 1))
 
