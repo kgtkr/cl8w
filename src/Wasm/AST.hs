65359e581ebe1e29ea5f6c260151687a243cb6dc
kgtkr <kgtkr.jp@gmail.com>
2018-07-20 15:24:16 +0900

    FuncCmd作成中

--- src/Wasm/AST.hs
+++ src/Wasm/AST.hs
@@ -8,3 +8,28 @@ data ValueType=I32|I64|F32|F64
 
 data Type=ValueType ValueType|AnyFunc|Func|BlockType (Maybe ValueType)
 
+type BlockType=Maybe ValueType
+
+data FuncCmd=
+    Unreachable
+    |Nop
+    |Block BlockType
+    |Loop BlockType
+    |If BlockType
+    |Else
+    |End
+    |Br Int
+    |BrIf Int
+    |BrTable Int [Int] Int
+    |Return
+    |Call Int
+    |CallIndirect Int Int
+    |Drop
+    |Select
+    |GetLocal Int
+    |SetLocal Int
+    |TeeLocal Int
+    |GetGlobal Int
+    |SetGlobal Int
+
+
