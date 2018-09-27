d4cdf4a0070b7310e078a0222e89f30841d28269
kgtkr <kgtkr.jp@gmail.com>
2018-09-27 23:41:49 +0900

    バグ修正

--- src/Wasm/Binary.hs
+++ src/Wasm/Binary.hs
@@ -235,265 +235,268 @@ instance WasmAST MemoryImmediate where
         putVaruint32 x
         putVaruint32 y
 
+putOpcode :: Putter Int
+putOpcode = putUint8
+
 instance WasmAST OperatorCode where
-    putWasmAST OpUnreachable=putUint32 0x00
-    putWasmAST OpNop=putUint32 0x01
+    putWasmAST OpUnreachable=putOpcode 0x00
+    putWasmAST OpNop=putOpcode 0x01
     putWasmAST (OpBlock x)=do
-        putUint32 0x02
+        putOpcode 0x02
         putWasmAST x
     putWasmAST (OpLoop x)=do
-        putUint32 0x03
+        putOpcode 0x03
         putWasmAST x
     putWasmAST (OpIf x)=do
-        putUint32 0x04
+        putOpcode 0x04
         putWasmAST x
-    putWasmAST OpElse=putUint32 0x05
-    putWasmAST OpEnd=putUint32 0x0b
+    putWasmAST OpElse=putOpcode 0x05
+    putWasmAST OpEnd=putOpcode 0x0b
     putWasmAST (OpBr x)=do
-        putUint32 0x0c
+        putOpcode 0x0c
         putVaruint32 x
     putWasmAST (OpBrIf x)=do
-        putUint32 0x0d
+        putOpcode 0x0d
         putVaruint32 x
     putWasmAST (OpBrTable x y)=do
-        putUint32 0x0e
+        putOpcode 0x0e
         putArray putVaruint32 x
         putVaruint32 y
-    putWasmAST OpReturn=putUint32 0x0f
+    putWasmAST OpReturn=putOpcode 0x0f
     putWasmAST (OpCall x)=do
-        putUint32 0x10
+        putOpcode 0x10
         putVaruint32 x
     putWasmAST (OpCallIndirect x)=do
-        putUint32 0x11
+        putOpcode 0x11
         putVaruint32 x
         putVaruint1 False
-    putWasmAST OpDrop=putUint32 0x1a
-    putWasmAST OpSelect=putUint32 0x1b
+    putWasmAST OpDrop=putOpcode 0x1a
+    putWasmAST OpSelect=putOpcode 0x1b
     putWasmAST (OpGetLocal x)=do
-        putUint32 0x20
+        putOpcode 0x20
         putVaruint32 x
     putWasmAST (OpSetLocal x)=do
-        putUint32 0x21
+        putOpcode 0x21
         putVaruint32 x
     putWasmAST (OpTeeLocal x)=do
-        putUint32 0x22
+        putOpcode 0x22
         putVaruint32 x
     putWasmAST (OpGetGlobal x)=do
-        putUint32 0x23
+        putOpcode 0x23
         putVaruint32 x
     putWasmAST (OpSetGlobal x)=do
-        putUint32 0x24
+        putOpcode 0x24
         putVaruint32 x
     putWasmAST (OpI32Load x)=do
-        putUint32 0x28
+        putOpcode 0x28
         putWasmAST x
     putWasmAST (OpI64Load x)=do
-        putUint32 0x29
+        putOpcode 0x29
         putWasmAST x
     putWasmAST (OpF32Load x)=do
-        putUint32 0x2a
+        putOpcode 0x2a
         putWasmAST x
     putWasmAST (OpF64Load x)=do
-        putUint32 0x2b
+        putOpcode 0x2b
         putWasmAST x
     putWasmAST (OpI32Load8s x)=do
-        putUint32 0x2c
+        putOpcode 0x2c
         putWasmAST x
     putWasmAST (OpI32Load8u x)=do
-        putUint32 0x2d
+        putOpcode 0x2d
         putWasmAST x
     putWasmAST (OpI32Load16s x)=do
-        putUint32 0x2e
+        putOpcode 0x2e
         putWasmAST x
     putWasmAST (OpI32Load16u x)=do
-        putUint32 0x2f
+        putOpcode 0x2f
         putWasmAST x
     putWasmAST (OpI64Load8s x)=do
-        putUint32 0x30
+        putOpcode 0x30
         putWasmAST x
     putWasmAST (OpI64Load8u x)=do
-        putUint32 0x31
+        putOpcode 0x31
         putWasmAST x
     putWasmAST (OpI64Load16s x)=do
-        putUint32 0x32
+        putOpcode 0x32
         putWasmAST x
     putWasmAST (OpI64Load16u x)=do
-        putUint32 0x33
+        putOpcode 0x33
         putWasmAST x
     putWasmAST (OpI64Load32s x)=do
-        putUint32 0x34
+        putOpcode 0x34
         putWasmAST x
     putWasmAST (OpI64Load32u x)=do
-        putUint32 0x35
+        putOpcode 0x35
         putWasmAST x
     putWasmAST (OpI32Store x)=do
-        putUint32 0x36
+        putOpcode 0x36
         putWasmAST x
     putWasmAST (OpI64Store x)=do
-        putUint32 0x37
+        putOpcode 0x37
         putWasmAST x
     putWasmAST (OpF32Store x)=do
-        putUint32 0x38
+        putOpcode 0x38
         putWasmAST x
     putWasmAST (OpF64Store x)=do
-        putUint32 0x39
+        putOpcode 0x39
         putWasmAST x
     putWasmAST (OpI32Store8 x)=do
-        putUint32 0x3a
+        putOpcode 0x3a
         putWasmAST x
     putWasmAST (OpI32Store16 x)=do
-        putUint32 0x3b
+        putOpcode 0x3b
         putWasmAST x
     putWasmAST (OpI64Store8 x)=do
-        putUint32 0x3c
+        putOpcode 0x3c
         putWasmAST x
     putWasmAST (OpI64Store16 x)=do
-        putUint32 0x3d
+        putOpcode 0x3d
         putWasmAST x
     putWasmAST (OpI64Store32 x)=do
-        putUint32 0x3e
+        putOpcode 0x3e
         putWasmAST x
     putWasmAST OpCurrentMemory=do
-        putUint32 0x3f
+        putOpcode 0x3f
         putVaruint1 False
     putWasmAST OpGrowMemory=do
-        putUint32 0x40
+        putOpcode 0x40
         putVaruint1 False
     putWasmAST (OpI32Const x)=do
-        putUint32 0x41
+        putOpcode 0x41
         putVarint32 x
     putWasmAST (OpI64Const x)=do
-        putUint32 0x42
+        putOpcode 0x42
         putVarint64 x
     putWasmAST (OpF32Const x)=do
-        putUint32 0x43
+        putOpcode 0x43
         putFloat32 x
     putWasmAST (OpF64Const x)=do
-        putUint32 0x44
+        putOpcode 0x44
         putFloat64 x
-    putWasmAST OpI32Eqz=putUint32 0x45
-    putWasmAST OpI32Eq=putUint32 0x46
-    putWasmAST OpI32Ne=putUint32 0x47
-    putWasmAST OpI32Lts=putUint32 0x48
-    putWasmAST OpI32Ltu=putUint32 0x49
-    putWasmAST OpI32Gts=putUint32 0x4a
-    putWasmAST OpI32Gtu=putUint32 0x4b
-    putWasmAST OpI32Les=putUint32 0x4c
-    putWasmAST OpI32Leu=putUint32 0x4d
-    putWasmAST OpI32Ges=putUint32 0x4e
-    putWasmAST OpI32Geu=putUint32 0x4f
-    putWasmAST OpI64Eqz=putUint32 0x50
-    putWasmAST OpI64Eq=putUint32 0x51
-    putWasmAST OpI64Ne=putUint32 0x52
-    putWasmAST OpI64Lts=putUint32 0x53
-    putWasmAST OpI64Ltu=putUint32 0x54
-    putWasmAST OpI64Gts=putUint32 0x55
-    putWasmAST OpI64Gtu=putUint32 0x56
-    putWasmAST OpI64Les=putUint32 0x57
-    putWasmAST OpI64Leu=putUint32 0x58
-    putWasmAST OpI64Ges=putUint32 0x59
-    putWasmAST OpI64Geu=putUint32 0x5a
-    putWasmAST OpF32Eq=putUint32 0x5b
-    putWasmAST OpF32Ne=putUint32 0x5c
-    putWasmAST OpF32Lt=putUint32 0x5d
-    putWasmAST OpF32Gt=putUint32 0x5e
-    putWasmAST OpF32Le=putUint32 0x5f
-    putWasmAST OpF32Ge=putUint32 0x60
-    putWasmAST OpF64Eq=putUint32 0x61
-    putWasmAST OpF64Ne=putUint32 0x62
-    putWasmAST OpF64Lt=putUint32 0x63
-    putWasmAST OpF64Gt=putUint32 0x64
-    putWasmAST OpF64Le=putUint32 0x65
-    putWasmAST OpF64Ge=putUint32 0x66
-    putWasmAST OpI32Clz=putUint32 0x67
-    putWasmAST OpI32Ctz=putUint32 0x68
-    putWasmAST OpI32Popcnt=putUint32 0x69
-    putWasmAST OpI32Add=putUint32 0x6a
-    putWasmAST OpI32Sub=putUint32 0x6b
-    putWasmAST OpI32Mul=putUint32 0x6c
-    putWasmAST OpI32Divs=putUint32 0x6d
-    putWasmAST OpI32Divu=putUint32 0x6e
-    putWasmAST OpI32Rems=putUint32 0x6f
-    putWasmAST OpI32Remu=putUint32 0x70
-    putWasmAST OpI32And=putUint32 0x71
-    putWasmAST OpI32Or=putUint32 0x72
-    putWasmAST OpI32Xor=putUint32 0x73
-    putWasmAST OpI32Shl=putUint32 0x74
-    putWasmAST OpI32Shrs=putUint32 0x75
-    putWasmAST OpI32Shru=putUint32 0x76
-    putWasmAST OpI32Rotl=putUint32 0x77
-    putWasmAST OpI32Rotr=putUint32 0x78
-    putWasmAST OpI64Clz=putUint32 0x79
-    putWasmAST OpI64Ctz=putUint32 0x7a
-    putWasmAST OpI64Popcnt=putUint32 0x7b
-    putWasmAST OpI64Add=putUint32 0x7c
-    putWasmAST OpI64Sub=putUint32 0x7d
-    putWasmAST OpI64Mul=putUint32 0x7e
-    putWasmAST OpI64Divs=putUint32 0x7f
-    putWasmAST OpI64Divu=putUint32 0x80
-    putWasmAST OpI64Rems=putUint32 0x81
-    putWasmAST OpI64Remu=putUint32 0x82
-    putWasmAST OpI64And=putUint32 0x83
-    putWasmAST OpI64Or=putUint32 0x84
-    putWasmAST OpI64Xor=putUint32 0x85
-    putWasmAST OpI64Shl=putUint32 0x86
-    putWasmAST OpI64Shrs=putUint32 0x87
-    putWasmAST OpI64Shru=putUint32 0x88
-    putWasmAST OpI64Rotl=putUint32 0x89
-    putWasmAST OpI64Rotr=putUint32 0x8a
-    putWasmAST OpF32Abs=putUint32 0x8b
-    putWasmAST OpF32Neg=putUint32 0x8c
-    putWasmAST OpF32Ceil=putUint32 0x8d
-    putWasmAST OpF32Floor=putUint32 0x8e
-    putWasmAST OpF32Trunc=putUint32 0x8f
-    putWasmAST OpF32Nearest=putUint32 0x90
-    putWasmAST OpF32Sqrt=putUint32 0x91
-    putWasmAST OpF32Add=putUint32 0x92
-    putWasmAST OpF32Sub=putUint32 0x93
-    putWasmAST OpF32Mul=putUint32 0x94
-    putWasmAST OpF32Div=putUint32 0x95
-    putWasmAST OpF32Min=putUint32 0x96
-    putWasmAST OpF32Max=putUint32 0x97
-    putWasmAST OpF32Copysign=putUint32 0x98
-    putWasmAST OpF64Abs=putUint32 0x99
-    putWasmAST OpF64Neg=putUint32 0x9a
-    putWasmAST OpF64Ceil=putUint32 0x9b
-    putWasmAST OpF64Floor=putUint32 0x9c
-    putWasmAST OpF64Trunc=putUint32 0x9d
-    putWasmAST OpF64Nearest=putUint32 0x9e
-    putWasmAST OpF64Sqrt=putUint32 0x9f
-    putWasmAST OpF64Add=putUint32 0xa0
-    putWasmAST OpF64Sub=putUint32 0xa1
-    putWasmAST OpF64Mul=putUint32 0xa2
-    putWasmAST OpF64Div=putUint32 0xa3
-    putWasmAST OpF64Min=putUint32 0xa4
-    putWasmAST OpF64Max=putUint32 0xa5
-    putWasmAST OpF64Copysign=putUint32 0xa6
-    putWasmAST OpI32WrapI64=putUint32 0xa7
-    putWasmAST OpI32TruncsF32=putUint32 0xa8
-    putWasmAST OpI32TrancuF32=putUint32 0xa9
-    putWasmAST OpI32TrancsF64=putUint32 0xaa
-    putWasmAST OpI32TrancuF64=putUint32 0xab
-    putWasmAST OpI64ExtendsI32=putUint32 0xac
-    putWasmAST OpI64ExtenduI32=putUint32 0xad
-    putWasmAST OpI64TruncsF32=putUint32 0xae
-    putWasmAST OpI64TrancuF32=putUint32 0xaf
-    putWasmAST OpI64TrancsF64=putUint32 0xb0
-    putWasmAST OpI64TrancuF64=putUint32 0xb1
-    putWasmAST OpF32ConvertsI32=putUint32 0xb2
-    putWasmAST OpF32ConvertuI32=putUint32 0xb3
-    putWasmAST OpF32ConvertsI64=putUint32 0xb4
-    putWasmAST OpF32ConvertuI64=putUint32 0xb5
-    putWasmAST OpF32DemoteF64=putUint32 0xb6
-    putWasmAST OpF64ConvertsI32=putUint32 0xb7
-    putWasmAST OpF64ConvertuI32=putUint32 0xb8
-    putWasmAST OpF64ConvertsI64=putUint32 0xb9
-    putWasmAST OpF64ConvertuI64=putUint32 0xba
-    putWasmAST OpF64PromoteF32=putUint32 0xbb
-    putWasmAST OpI32ReinterpretF32=putUint32 0xbc
-    putWasmAST OpI64ReinterpretF64=putUint32 0xbd
-    putWasmAST OpF32ReinterpretI32=putUint32 0xbe
-    putWasmAST OpF64ReinterpretI64=putUint32 0xbf
+    putWasmAST OpI32Eqz=putOpcode 0x45
+    putWasmAST OpI32Eq=putOpcode 0x46
+    putWasmAST OpI32Ne=putOpcode 0x47
+    putWasmAST OpI32Lts=putOpcode 0x48
+    putWasmAST OpI32Ltu=putOpcode 0x49
+    putWasmAST OpI32Gts=putOpcode 0x4a
+    putWasmAST OpI32Gtu=putOpcode 0x4b
+    putWasmAST OpI32Les=putOpcode 0x4c
+    putWasmAST OpI32Leu=putOpcode 0x4d
+    putWasmAST OpI32Ges=putOpcode 0x4e
+    putWasmAST OpI32Geu=putOpcode 0x4f
+    putWasmAST OpI64Eqz=putOpcode 0x50
+    putWasmAST OpI64Eq=putOpcode 0x51
+    putWasmAST OpI64Ne=putOpcode 0x52
+    putWasmAST OpI64Lts=putOpcode 0x53
+    putWasmAST OpI64Ltu=putOpcode 0x54
+    putWasmAST OpI64Gts=putOpcode 0x55
+    putWasmAST OpI64Gtu=putOpcode 0x56
+    putWasmAST OpI64Les=putOpcode 0x57
+    putWasmAST OpI64Leu=putOpcode 0x58
+    putWasmAST OpI64Ges=putOpcode 0x59
+    putWasmAST OpI64Geu=putOpcode 0x5a
+    putWasmAST OpF32Eq=putOpcode 0x5b
+    putWasmAST OpF32Ne=putOpcode 0x5c
+    putWasmAST OpF32Lt=putOpcode 0x5d
+    putWasmAST OpF32Gt=putOpcode 0x5e
+    putWasmAST OpF32Le=putOpcode 0x5f
+    putWasmAST OpF32Ge=putOpcode 0x60
+    putWasmAST OpF64Eq=putOpcode 0x61
+    putWasmAST OpF64Ne=putOpcode 0x62
+    putWasmAST OpF64Lt=putOpcode 0x63
+    putWasmAST OpF64Gt=putOpcode 0x64
+    putWasmAST OpF64Le=putOpcode 0x65
+    putWasmAST OpF64Ge=putOpcode 0x66
+    putWasmAST OpI32Clz=putOpcode 0x67
+    putWasmAST OpI32Ctz=putOpcode 0x68
+    putWasmAST OpI32Popcnt=putOpcode 0x69
+    putWasmAST OpI32Add=putOpcode 0x6a
+    putWasmAST OpI32Sub=putOpcode 0x6b
+    putWasmAST OpI32Mul=putOpcode 0x6c
+    putWasmAST OpI32Divs=putOpcode 0x6d
+    putWasmAST OpI32Divu=putOpcode 0x6e
+    putWasmAST OpI32Rems=putOpcode 0x6f
+    putWasmAST OpI32Remu=putOpcode 0x70
+    putWasmAST OpI32And=putOpcode 0x71
+    putWasmAST OpI32Or=putOpcode 0x72
+    putWasmAST OpI32Xor=putOpcode 0x73
+    putWasmAST OpI32Shl=putOpcode 0x74
+    putWasmAST OpI32Shrs=putOpcode 0x75
+    putWasmAST OpI32Shru=putOpcode 0x76
+    putWasmAST OpI32Rotl=putOpcode 0x77
+    putWasmAST OpI32Rotr=putOpcode 0x78
+    putWasmAST OpI64Clz=putOpcode 0x79
+    putWasmAST OpI64Ctz=putOpcode 0x7a
+    putWasmAST OpI64Popcnt=putOpcode 0x7b
+    putWasmAST OpI64Add=putOpcode 0x7c
+    putWasmAST OpI64Sub=putOpcode 0x7d
+    putWasmAST OpI64Mul=putOpcode 0x7e
+    putWasmAST OpI64Divs=putOpcode 0x7f
+    putWasmAST OpI64Divu=putOpcode 0x80
+    putWasmAST OpI64Rems=putOpcode 0x81
+    putWasmAST OpI64Remu=putOpcode 0x82
+    putWasmAST OpI64And=putOpcode 0x83
+    putWasmAST OpI64Or=putOpcode 0x84
+    putWasmAST OpI64Xor=putOpcode 0x85
+    putWasmAST OpI64Shl=putOpcode 0x86
+    putWasmAST OpI64Shrs=putOpcode 0x87
+    putWasmAST OpI64Shru=putOpcode 0x88
+    putWasmAST OpI64Rotl=putOpcode 0x89
+    putWasmAST OpI64Rotr=putOpcode 0x8a
+    putWasmAST OpF32Abs=putOpcode 0x8b
+    putWasmAST OpF32Neg=putOpcode 0x8c
+    putWasmAST OpF32Ceil=putOpcode 0x8d
+    putWasmAST OpF32Floor=putOpcode 0x8e
+    putWasmAST OpF32Trunc=putOpcode 0x8f
+    putWasmAST OpF32Nearest=putOpcode 0x90
+    putWasmAST OpF32Sqrt=putOpcode 0x91
+    putWasmAST OpF32Add=putOpcode 0x92
+    putWasmAST OpF32Sub=putOpcode 0x93
+    putWasmAST OpF32Mul=putOpcode 0x94
+    putWasmAST OpF32Div=putOpcode 0x95
+    putWasmAST OpF32Min=putOpcode 0x96
+    putWasmAST OpF32Max=putOpcode 0x97
+    putWasmAST OpF32Copysign=putOpcode 0x98
+    putWasmAST OpF64Abs=putOpcode 0x99
+    putWasmAST OpF64Neg=putOpcode 0x9a
+    putWasmAST OpF64Ceil=putOpcode 0x9b
+    putWasmAST OpF64Floor=putOpcode 0x9c
+    putWasmAST OpF64Trunc=putOpcode 0x9d
+    putWasmAST OpF64Nearest=putOpcode 0x9e
+    putWasmAST OpF64Sqrt=putOpcode 0x9f
+    putWasmAST OpF64Add=putOpcode 0xa0
+    putWasmAST OpF64Sub=putOpcode 0xa1
+    putWasmAST OpF64Mul=putOpcode 0xa2
+    putWasmAST OpF64Div=putOpcode 0xa3
+    putWasmAST OpF64Min=putOpcode 0xa4
+    putWasmAST OpF64Max=putOpcode 0xa5
+    putWasmAST OpF64Copysign=putOpcode 0xa6
+    putWasmAST OpI32WrapI64=putOpcode 0xa7
+    putWasmAST OpI32TruncsF32=putOpcode 0xa8
+    putWasmAST OpI32TrancuF32=putOpcode 0xa9
+    putWasmAST OpI32TrancsF64=putOpcode 0xaa
+    putWasmAST OpI32TrancuF64=putOpcode 0xab
+    putWasmAST OpI64ExtendsI32=putOpcode 0xac
+    putWasmAST OpI64ExtenduI32=putOpcode 0xad
+    putWasmAST OpI64TruncsF32=putOpcode 0xae
+    putWasmAST OpI64TrancuF32=putOpcode 0xaf
+    putWasmAST OpI64TrancsF64=putOpcode 0xb0
+    putWasmAST OpI64TrancuF64=putOpcode 0xb1
+    putWasmAST OpF32ConvertsI32=putOpcode 0xb2
+    putWasmAST OpF32ConvertuI32=putOpcode 0xb3
+    putWasmAST OpF32ConvertsI64=putOpcode 0xb4
+    putWasmAST OpF32ConvertuI64=putOpcode 0xb5
+    putWasmAST OpF32DemoteF64=putOpcode 0xb6
+    putWasmAST OpF64ConvertsI32=putOpcode 0xb7
+    putWasmAST OpF64ConvertuI32=putOpcode 0xb8
+    putWasmAST OpF64ConvertsI64=putOpcode 0xb9
+    putWasmAST OpF64ConvertuI64=putOpcode 0xba
+    putWasmAST OpF64PromoteF32=putOpcode 0xbb
+    putWasmAST OpI32ReinterpretF32=putOpcode 0xbc
+    putWasmAST OpI64ReinterpretF64=putOpcode 0xbd
+    putWasmAST OpF32ReinterpretI32=putOpcode 0xbe
+    putWasmAST OpF64ReinterpretI64=putOpcode 0xbf
 
 instance WasmAST WasmASTRoot where
     putWasmAST (WasmASTRoot a b c d e f g h i j k)=do
