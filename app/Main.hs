58fba3c687081ea65ade158d8ce53f4b9c0199ec
kgtkr <kgtkr.jp@gmail.com>
2018-09-27 22:54:40 +0900

    うーん

--- app/Main.hs
+++ app/Main.hs
@@ -1,1 +1,22 @@
-main = print "hoge"
+import           Wasm.AST
+import           Wasm.Binary
+import           Data.Serialize
+import qualified Data.ByteString               as BS
+
+ast :: WasmASTRoot
+ast = WasmASTRoot (Just (TypeSection [FuncType [ValI32] (Just ValI32)]))
+                  Nothing
+                  Nothing
+                  Nothing
+                  Nothing
+                  Nothing
+                  Nothing
+                  Nothing
+                  Nothing
+                  Nothing
+                  Nothing
+
+main = do
+    let bin = runPut $ putWasmAST ast
+    BS.writeFile "test.wasm" bin
+    return ()
