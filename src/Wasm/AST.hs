145def0c350be0c241876f2da4ea07fbcc47ffda
kgtkr <kgtkr.jp@gmail.com>
2018-09-27 13:34:02 +0900

    シリアライズ型

--- src/Wasm/AST.hs
+++ src/Wasm/AST.hs
@@ -1,8 +1,6 @@
 module Wasm.AST where
-import qualified Data.Binary                   as B
 import qualified Data.ByteString               as BS
 import           Data.Int
-import           Data.Binary.Get
 
 data ValueType = I32|I64|F32|F64
 
--- src/Wasm/Binary.hs
+++ src/Wasm/Binary.hs
@@ -1,7 +1,39 @@
 module Wasm.Binary where
 
 import           Wasm.AST
+import           Data.Int
+import           Data.Word
+import qualified Data.Bytes.Signed             as S
+import           Data.Serialize.Put
+import           Data.Serialize
+import           Data.Bytes.VarInt
+import qualified Data.Bytes.Put                as P
+import           Data.Bytes.Serial              ( serialize )
 
-opCode :: Code -> Int
-opCode Unreachable = 0x00
-opCode Nop         = 0x01
+putUint8 :: Putter Int
+putUint8 = putWord8 . fromIntegral
+
+putUint16 :: Putter Int
+putUint16 = putWord16le . fromIntegral
+
+putUint32 :: Putter Int
+putUint32 = putWord32le . fromIntegral
+
+putVaruint1 :: Putter Bool
+putVaruint1 True  = serialize (1 :: VarInt Word8)
+putVaruint1 False = serialize (0 :: VarInt Word8)
+
+putVaruint7 :: Putter Int
+putVaruint7 x = serialize (fromIntegral x :: VarInt Word8)
+
+putVaruint32 :: Putter Int
+putVaruint32 x = serialize (fromIntegral x :: VarInt Word32)
+
+putVarint7 :: Putter Int
+putVarint7 x = serialize (fromIntegral x :: VarInt Int8)
+
+putVarint32 :: Putter Int
+putVarint32 x = serialize (fromIntegral x :: VarInt Int32)
+
+putVarint64 :: Putter Int
+putVarint64 x = serialize (fromIntegral x :: VarInt Int64)
