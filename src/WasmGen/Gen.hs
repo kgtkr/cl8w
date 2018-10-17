1b735d415807a282e220eb4aedefd33a257907a8
kgtkr <kgtkr.jp@gmail.com>
2018-10-17 18:03:38 +0900

    memberMap

--- src/WasmGen/Gen.hs
+++ src/WasmGen/Gen.hs
@@ -5,9 +5,11 @@ import qualified Parsers.Expr                  as PE
 import qualified Data.Map                      as M
 import qualified Parsers.Lang                  as L
 
-import           Parsers.Member                 ( StructMembers )
+import qualified Parsers.Member                as Me
+import           Data.Tuple                     ( swap )
+import           Data.Maybe                     ( mapMaybe )
 
-type MemberMap=(M.Map String Int,M.Map String StructMembers)
+type MemberMap=(M.Map String Int,M.Map String Me.StructMembers)
 
 sizeOf :: L.Type -> Int
 sizeOf L.TI32  = 4
@@ -18,3 +20,14 @@ sizeOf L.TBool = 4
 sizeOf L.TChar = 4
 sizeOf _       = 4
 
+memberMap :: [Me.Member] -> MemberMap
+memberMap m =
+    ( (M.fromList . map swap . zip [0 ..] . mapMaybe fnMap) m
+    , (M.fromList . mapMaybe stMap) m
+    )
+  where
+    fnMap (Me.MFun (Me.FuncDef name _ _) _) = Just name
+    fnMap (Me.MExternFun (Me.FuncDef name _ _) _) = Just name
+    fnMap _ = Nothing
+    stMap (Me.MStruct a b) = Just (a, b)
+    stMap _                = Nothing
