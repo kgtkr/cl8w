5213af40f22a0290295bb628556cc114656abbdf
kgtkr <kgtkr.jp@gmail.com>
2018-10-21 15:46:04 +0900

    local系

--- src/WasmGen/Expr.hs
+++ src/WasmGen/Expr.hs
@@ -6,13 +6,16 @@ import qualified Data.DList                    as D
 import qualified Wasm.AST                      as W
 import qualified Data.Map                      as M
 import           Control.Monad.State
+
+import qualified Parsers.Lang                  as L
+
 data ExprGenData=ExprGenData OpCodes Locals LocalsLen LocalsMap
 
 type OpCodes=D.DList W.OperatorCode
 type Locals=D.DList W.ValueType
 type LocalsLen=Int
-type LocalsMap=M.Map String Int
-
+type LocalsMap=M.Map String LocalData
+type LocalData=(W.ValueType,Int)
 instance Monoid ExprGenData where
     mempty = ExprGenData mempty mempty 0 mempty
     ExprGenData a1 a2 a3 a4 `mappend` ExprGenData b1 b2 b3 b4=ExprGenData (mappend a1 b1) (mappend a2 b2) (a3 + b3) (mempty a4 b4)
@@ -79,9 +82,20 @@ modifyLocalsMap f = do
     x <- getLocalsMap
     putLocalsMap $ f x
 
-addLocal :: W.ValueType -> ExprGen LocalsLen
+addLocal :: W.ValueType -> ExprGen Int
 addLocal t = do
     len <- getLocalsLen
     modifyLocalsLen (+ 1)
     modifyLocals $ (flip D.snoc) t
     return len
+
+addNamedLocal :: W.ValueType -> L.Ident -> ExprGen Int
+addNamedLocal t name = do
+    id <- addLocal t
+    modifyLocalsMap (M.insert name (t, id))
+    return id
+
+getNamedLocal :: L.Ident -> ExprGen LocalData
+getNamedLocal name = do
+    x <- getLocalsMap
+    return $ x M.! name
