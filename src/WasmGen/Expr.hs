fd16a4762887b665a4aa3c37063267de46ac33b3
kgtkr <kgtkr.jp@gmail.com>
2018-10-21 17:02:27 +0900

    色々追加

--- src/WasmGen/Expr.hs
+++ src/WasmGen/Expr.hs
@@ -1,36 +1,41 @@
 module WasmGen.Expr where
 
-import           Data.Monoid
-import           Data.Semigroup
 import qualified Data.DList                    as D
 import qualified Wasm.AST                      as W
 import qualified Data.Map                      as M
 import           Control.Monad.State
-
+import qualified Parsers.Member                as Me
 import qualified Parsers.Lang                  as L
 
-data ExprGenData=ExprGenData OpCodes Locals LocalsLen LocalsMap
-
+data ExprGenData=ExprGenData OpCodes Locals LocalsLen LocalsMap FunctionMap StructMap
+type FunctionMap=M.Map String Int
+type StructMap=M.Map String Me.StructMembers
 type OpCodes=D.DList W.OperatorCode
 type Locals=D.DList W.ValueType
 type LocalsLen=Int
 type LocalsMap=M.Map String LocalData
 type LocalData=(W.ValueType,Int)
-instance Monoid ExprGenData where
-    mempty = ExprGenData mempty mempty 0 mempty
-    ExprGenData a1 a2 a3 a4 `mappend` ExprGenData b1 b2 b3 b4=ExprGenData (mappend a1 b1) (mappend a2 b2) (a3 + b3) (mempty a4 b4)
-
 type ExprGen = State ExprGenData
+
+getFunctions :: ExprGen FunctionMap
+getFunctions = do
+    (ExprGenData _ _ _ _ x _) <- get
+    return x
+
+getStructs :: ExprGen StructMap
+getStructs = do
+    (ExprGenData _ _ _ _ _ x) <- get
+    return x
 
 getOpCodes :: ExprGen OpCodes
 getOpCodes = do
-    (ExprGenData x _ _ _) <- get
+    (ExprGenData x _ _ _ _ _) <- get
     return x
 
 putOpCpdes :: OpCodes -> ExprGen ()
 putOpCpdes x = do
-    (ExprGenData _ a b c) <- get
-    put $ ExprGenData x a b c
+    (ExprGenData _ a b c d e) <- get
+    put $ ExprGenData x a b c d e
 
 modifyOpCodes :: (OpCodes -> OpCodes) -> ExprGen ()
 modifyOpCodes f = do
@@ -39,13 +44,13 @@ modifyOpCodes f = do
 
 getLocals :: ExprGen Locals
 getLocals = do
-    (ExprGenData _ x _ _) <- get
+    (ExprGenData _ x _ _ _ _) <- get
     return x
 
 putLocals :: Locals -> ExprGen ()
 putLocals x = do
-    (ExprGenData a _ b c) <- get
-    put $ ExprGenData a x b c
+    (ExprGenData a _ b c d e) <- get
+    put $ ExprGenData a x b c d e
 
 modifyLocals :: (Locals -> Locals) -> ExprGen ()
 modifyLocals f = do
@@ -54,13 +59,13 @@ modifyLocals f = do
 
 getLocalsLen :: ExprGen LocalsLen
 getLocalsLen = do
-    (ExprGenData _ _ x _) <- get
+    (ExprGenData _ _ x _ _ _) <- get
     return x
 
 putLocalsLen :: LocalsLen -> ExprGen ()
 putLocalsLen x = do
-    (ExprGenData a b _ c) <- get
-    put $ ExprGenData a b x c
+    (ExprGenData a b _ c d e) <- get
+    put $ ExprGenData a b x c d e
 
 modifyLocalsLen :: (LocalsLen -> LocalsLen) -> ExprGen ()
 modifyLocalsLen f = do
@@ -69,13 +74,13 @@ modifyLocalsLen f = do
 
 getLocalsMap :: ExprGen LocalsMap
 getLocalsMap = do
-    (ExprGenData _ _ _ x) <- get
+    (ExprGenData _ _ _ x _ _) <- get
     return x
 
 putLocalsMap :: LocalsMap -> ExprGen ()
 putLocalsMap x = do
-    (ExprGenData a b c _) <- get
-    put $ ExprGenData a b c x
+    (ExprGenData a b c _ d e) <- get
+    put $ ExprGenData a b c x d e
 
 modifyLocalsMap :: (LocalsMap -> LocalsMap) -> ExprGen ()
 modifyLocalsMap f = do
