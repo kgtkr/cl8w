c3a8f8069b64383d3f92b4a52830234f5b038ac3
kgtkr <kgtkr.jp@gmail.com>
2018-10-21 13:49:46 +0900

    ExprGenData修正

--- src/WasmGen/Expr.hs
+++ src/WasmGen/Expr.hs
@@ -4,10 +4,12 @@ import           Data.Monoid
 import           Data.Semigroup
 import qualified Data.DList                    as D
 import qualified Wasm.AST                      as W
+import qualified Data.Map                      as M
 
-data ExprGenData=ExprGenData (D.DList W.OperatorCode) (D.DList W.ValueType)
+-- コード,ローカル変数リスト,ローカル変数の長さ,ローカル変数名
+data ExprGenData=ExprGenData (D.DList W.OperatorCode) (D.DList W.ValueType) Int (M.Map String Int)
 
 instance Monoid ExprGenData where
-    mempty = ExprGenData mempty mempty
-    ExprGenData a1 a2 `mappend` ExprGenData b1 b2=ExprGenData (mappend a1 b1) (mappend a2 b2)
+    mempty = ExprGenData mempty mempty 0 mempty
+    ExprGenData a1 a2 a3 a4 `mappend` ExprGenData b1 b2 b3 b4=ExprGenData (mappend a1 b1) (mappend a2 b2) (a3 + b3) (mempty a4 b4)
 
