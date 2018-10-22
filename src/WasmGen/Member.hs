91544991b5d10f1ac9b1e5861d3f8a335bfefb1f
kgtkr <kgtkr.jp@gmail.com>
2018-10-22 23:54:34 +0900

    Struct

--- src/WasmGen/Member.hs
+++ src/WasmGen/Member.hs
@@ -6,6 +6,7 @@ import qualified Parsers.Lang                  as L
 import           Control.Lens
 import qualified Data.Map                      as M
 import qualified Parsers.Member                as Me
+import           WasmGen.Lang
 
 type FunctionMap=M.Map String (Int,Me.FuncDef)
 type StructMap=M.Map String Me.StructMembers
@@ -16,3 +17,14 @@ data MemberData=MemberData{
 }
 
 makeLenses ''MemberData
+
+
+data StructProps=StructProps{
+    _pos::Int,
+    _typ::L.Type,
+    _name::String
+}
+
+makeLenses ''StructProps
+
+type Struct=M.Map String StructProps
