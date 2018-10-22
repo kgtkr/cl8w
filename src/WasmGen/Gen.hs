55750c2931b2f9f36c77d92f7190aa590cf6521c
kgtkr <kgtkr.jp@gmail.com>
2018-10-22 17:39:52 +0900

    余計なコード削除

--- src/WasmGen/Gen.hs
+++ src/WasmGen/Gen.hs
@@ -28,35 +28,3 @@ memberMap m =
     stMap _                = Nothing
 
 type FunctionMap=M.Map String Int
-
-type LocalVarMap=M.Map String (Int,L.Type)
-
-type OpCodeWriter = Writer (D.DList W.OperatorCode) ()
-
-tellOp :: W.OperatorCode -> OpCodeWriter
-tellOp = tell . pure
-
-callGen :: FunctionMap -> String -> [OpCodeWriter] -> OpCodeWriter
-callGen map f args = do
-    sequence_ args
-    tellOp $ W.OpCall $ map M.! f
-    return ()
-
-blockGen :: W.BlockType -> OpCodeWriter -> OpCodeWriter
-blockGen t x = do
-    tell $ pure $ W.OpBlock t
-    x
-    tell $ pure $ W.OpEnd
-
-exprGen :: MemberMap -> LocalVarMap -> E.Expr -> OpCodeWriter
-exprGen mMap@(fMap, sMap) lMap expr = case expr of
-    E.EStructL name exprs -> blockGen (W.BlockType (Just W.ValI32)) $ do
-        let sDef    = map fst $ sMap M.! name
-        let exprMap = M.fromList exprs
-        callGen fMap
-                (name ++ ":new")
-                (map (exprGen mMap lMap . (exprMap M.!)) sDef)
-    E.EI32L x -> tellOp $ W.OpI32Const x
-    E.EI64L x -> tellOp $ W.OpI64Const x
-    E.EF32L x -> tellOp $ W.OpF32Const x
-    E.EF64L x -> tellOp $ W.OpF64Const x
