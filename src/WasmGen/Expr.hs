25c0198d45166b7b4a33d1c7c3e7b5ed0bc58c5f
kgtkr <kgtkr.jp@gmail.com>
2018-10-23 01:09:12 +0900

    構造体リテラル

--- src/WasmGen/Expr.hs
+++ src/WasmGen/Expr.hs
@@ -35,16 +35,16 @@ makeLenses ''ExprGenData
 
 type ExprGen = State ExprGenData
 
-addLocal :: L.Type -> ExprGen Int
+addLocal :: W.ValueType -> ExprGen Int
 addLocal t = do
     len <- use localsLen
     localsLen += 1
-    locals %= (flip D.snoc . WL.typeToValueType) t
+    locals %= (flip D.snoc) t
     return len
 
 addNamedLocalData :: L.Type -> L.Ident -> ExprGen Int
 addNamedLocalData t name = do
-    id <- addLocal t
+    id <- (addLocal . WL.typeToValueType) t
     localsMap %= (M.insert name (t, id))
     return id
 
@@ -53,10 +53,14 @@ addOpCode x = opCodes %= (flip D.snoc) x
 
 callGen :: WM.FunctionMap -> String -> [ExprGen ()] -> ExprGen ()
 callGen m f args = do
-    sequence_ args
     let (id, Me.FuncDef _ _ re) = m M.! f
-    addOpCode $ W.OpCall id
+    opCallGen (W.OpCall id) args
     exprType .= re
+
+opCallGen :: W.OperatorCode -> [ExprGen ()] -> ExprGen ()
+opCallGen op args = do
+    sequence_ args
+    addOpCode op
 
 blockGen :: W.BlockType -> ExprGen () -> ExprGen ()
 blockGen t x = do
@@ -72,9 +76,24 @@ exprGen expr = case expr of
     E.EStructL name exprs -> blockGen (W.BlockType (Just W.ValI32)) $ do
         fMap <- use functionMap
         sDef <- (M.! name) <$> use structMap
+        res  <- addLocal W.ValI32
         callGen fMap "malloc" [addOpCode $ W.OpI32Const (WM.structSize sDef)]
-        -- TODO 式の値を書き込む
+        mapM_
+            (\(ident, ex) -> do
+                let prop = sDef M.! ident
+                let storeOp =
+                        (case WL.typeToValueType (WM._typ prop) of
+                                W.ValI32 -> W.OpI32Store
+                                W.ValI64 -> W.OpI64Store
+                                W.ValF32 -> W.OpF32Store
+                                W.ValF64 -> W.OpF64Store
+                            )
+                            (W.MemoryImmediate 2 (WM._pos prop))
+                opCallGen storeOp [addOpCode $ W.OpGetLocal res, exprGen ex]
+            )
+            exprs
         exprType .= (Just . L.RefType . L.TStruct) name
+        addOpCode $ W.OpGetLocal res
     E.EI32L x -> addOpCode $ W.OpI32Const x
     E.EI64L x -> addOpCode $ W.OpI64Const x
     E.EF32L x -> addOpCode $ W.OpF32Const x
