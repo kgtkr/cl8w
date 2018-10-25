b3e23d4408ecfbd07a4df270d290f4918999162e
kgtkr <kgtkr.jp@gmail.com>
2018-10-25 22:16:10 +0900

    実行

---
+++ test.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+const buf = fs.readFileSync("./test.wasm");
+const mod = new WebAssembly.Module(buf);
+const instance = new WebAssembly.Instance(mod, {});
+console.log(instance.exports.gcd(10, 20));
