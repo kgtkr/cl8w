3236b15ad3986a9da70c1a8711779637502eee39
kgtkr <kgtkr.jp@gmail.com>
2018-10-26 18:05:58 +0900

    rename

--- src/Gen/Gen.hs
+++ src/Gen/Gen.hs
@@ -1,11 +1,8 @@
 module Gen.Gen where
 
-import qualified Wasm.AST                      as W
-import qualified Parsers.Expr                  as E
 import qualified Data.Map                      as M
-import qualified Parsers.Lang                  as L
 
-import qualified Parsers.Member                as Me
+import qualified Parsers.Member                as PM
 import           Data.Tuple                     ( swap )
 import           Data.Maybe                     ( mapMaybe )
 import qualified Data.DList                    as D
@@ -13,18 +10,18 @@ import           Control.Monad.Writer
 
 type MemberMap=(FunctionMap,StructMap)
 
-type StructMap=M.Map String Me.StructMembers
+type StructMap=M.Map String PM.StructMembers
 
-memberMap :: [Me.Member] -> MemberMap
+memberMap :: [PM.Member] -> MemberMap
 memberMap m =
   ( (M.fromList . map swap . zip [0 ..] . mapMaybe fnMap) m
   , (M.fromList . mapMaybe stMap) m
   )
  where
-  fnMap (Me.MFun (Me.FuncDef name _ _) _) = Just name
-  fnMap (Me.MExternFun (Me.FuncDef name _ _) _) = Just name
+  fnMap (PM.MFun (PM.FuncDef name _ _) _) = Just name
+  fnMap (PM.MExternFun (PM.FuncDef name _ _) _) = Just name
   fnMap _ = Nothing
-  stMap (Me.MStruct a b) = Just (a, b)
+  stMap (PM.MStruct a b) = Just (a, b)
   stMap _                = Nothing
 
 type FunctionMap=M.Map String Int
