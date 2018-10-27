50ae97224e98559564c3421bd5fd74cfabfffa73
kgtkr <kgtkr.jp@gmail.com>
2018-10-27 22:34:13 +0900

    バグ修正

--- test.js
+++ test.js
@@ -2,5 +2,18 @@ const fs = require("fs");
 
 const buf = fs.readFileSync("./test.wasm");
 const mod = new WebAssembly.Module(buf);
-const instance = new WebAssembly.Instance(mod, {});
+const instance = new WebAssembly.Instance(mod, {
+  resource: {
+    memory: new WebAssembly.Memory({ initial: 1 })
+  },
+  memory: new WebAssembly.Instance(new WebAssembly.Module(fs.readFileSync("./memory.wasm")), {
+    resource: {
+      memory: new WebAssembly.Memory({ initial: 1 })
+    },
+    config: {
+      start: 1
+    }
+  }).exports
+
+});
 console.log(instance.exports.gcd(24, 36));
